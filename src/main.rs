d186238f931d75f1e7dae14549a1c518a32d45ad
Masaki Fujiwara <m-fujiwara@seesaa.co.jp>
2017-12-26 23:04:48 +0900

    Add hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate iron;
+use iron::{Iron, IronResult, Request, Response, status, middleware};
+
+struct Hello;
+
+impl middleware::Handler for Hello {
+    fn handle(&self, _: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hello, world!")))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(Hello).http("0.0.0.0:3000").unwrap();
 }
