3b06d9a11ce3b5a182b59fc6f2ac73389dbcdda7
Masaki Fujiwara <m-fujiwara@seesaa.co.jp>
2017-12-27 16:54:13 +0900

    Add chain

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,40 @@
 extern crate iron;
 extern crate router;
 
+use iron::middleware::Chain;
 use iron::{Iron, IronResult, Request, Response, status, middleware};
 use router::Router;
+
+use iron::{BeforeMiddleware, AfterMiddleware};
+use iron::headers::{Authorization, Bearer};
+use iron::error::{IronError, HttpError};
+
+struct AuthorizationMiddleware;
+
+impl BeforeMiddleware for AuthorizationMiddleware {
+
+    fn before(&self, req: &mut Request) -> IronResult<()> {
+        if let Some(&Authorization(Bearer{ ref token })) = req.headers.get() {
+            println!("{}", token);
+            if token == "hoge" {
+                return Ok(())
+            }
+        }
+        Err(IronError::new(HttpError::Header, (status::Unauthorized)))
+    }
+}
+
+impl AfterMiddleware for AuthorizationMiddleware {
+
+    fn catch(&self, _: &mut Request, err: IronError) -> IronResult<Response> {
+        match err.response.status {
+            Some(status::Unauthorized) => {
+                Ok(Response::with((status::Unauthorized, "401 unauthorized")))
+            },
+            _ => Err(err)
+        }
+    }
+}
 
 struct Hello;
 
@@ -22,5 +54,8 @@ fn main() {
     router.get("/hello", Hello, "hello");
     router.post("/hoge", hoge, "hoge");
 
-    Iron::new(router).http("0.0.0.0:3000").unwrap();
+    let mut chain = Chain::new(router);
+    chain.link((AuthorizationMiddleware, AuthorizationMiddleware));
+
+    Iron::new(chain).http("0.0.0.0:3000").unwrap();
 }
